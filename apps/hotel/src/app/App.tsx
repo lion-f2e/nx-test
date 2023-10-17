3c6893e662d130f1c65694b0f83853c4d80f27de
Haku <hakudevtw@gmail.com>
2023-10-17 13:50:36 +0800

    test: extract test file to separate test file

--- apps/hotel/src/app/App.tsx
+++ apps/hotel/src/app/App.tsx
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from "./App.module.css";
-
 export function App() {
   return (
     <div>
@@ -13,26 +10,3 @@ export function App() {
 }
 
 export default App;
-
-if (import.meta.vitest) {
-  // add tests related to your file here
-  // For more information please visit the Vitest docs site here: https://vitest.dev/guide/in-source.html
-
-  const { it, expect, beforeEach } = import.meta.vitest;
-  // eslint-disable-next-line
-  let render: any;
-
-  beforeEach(async () => {
-    render = (await import("@testing-library/react")).render;
-  });
-
-  it("should render successfully", () => {
-    const { baseElement } = render(<App />);
-    expect(baseElement).toBeTruthy();
-  });
-
-  it("should have a greeting as the title", () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Welcome hotel/gi)).toBeTruthy();
-  });
-}
