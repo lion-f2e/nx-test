2901c432cd6db3d6849a59b0617484743e4d3acd
Haku <hakudevtw@gmail.com>
2023-10-17 18:10:54 +0800

    feat: add utils library
    1. clsx + taiwing-merge -> cN

--- apps/hotel/src/app/App.tsx
+++ apps/hotel/src/app/App.tsx
@@ -1,10 +1,11 @@
 import { HomeLayout } from "@shared-features/home";
+import { cN } from "@utils/tailwind";
 import navigation from "../constants/navigation";
 
 export function App() {
   return (
     <div>
-      <h1 className="px-2">
+      <h1 className={cN("px-2")}>
         <HomeLayout title="訂房的小小世界" navigation={navigation} />
         <span> Hello there, </span>
         Welcome hotel 👋
