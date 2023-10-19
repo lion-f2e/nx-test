ec48a276067dd89bc6663a4c27a61805b691e758
Yue <davidpchen@liontravel.com>
2023-10-19 16:18:30 +0800

    refactor: refactor vite config in project

--- apps/hotel/vite.config.ts
+++ apps/hotel/vite.config.ts
@@ -1,5 +1,30 @@
-import viteHandler from "../../configs/vite";
+import { defineConfig } from "vite";
+import {
+  plugins,
+  getTestOptions,
+  getBuildOptions,
+  getPreviewOptions,
+  getServeOptions,
+} from "../../configs/vite";
 
 const pageNames = ["search"];
 
-export default viteHandler(__dirname, pageNames);
+export default defineConfig({
+  define: { "import.meta.vitest": undefined },
+  cacheDir: "../../node_modules/.vite/hotel",
+
+  plugins: [...plugins],
+
+  server: { ...getServeOptions() },
+
+  build: { ...getBuildOptions(__dirname, pageNames) },
+
+  preview: { ...getPreviewOptions() },
+
+  test: { ...getTestOptions() },
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+});
--- configs/vite/index.ts
+++ configs/vite/index.ts
@@ -1,43 +1,28 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import importToCDN from "vite-plugin-cdn-import"; // https://github.com/MMF-FE/vite-plugin-cdn-import
-import getTestOptions from "./test";
-import getBuildOptions from "./build";
-import getPreviewOptions from "./preview";
-import getServeOptions from "./serve";
 
-export default function (rootDir: string, pageNames: string[]) {
-  return defineConfig({
-    define: { "import.meta.vitest": undefined },
-    cacheDir: "../../node_modules/.vite/hotel",
-    plugins: [
-      react(),
-      tsconfigPaths(),
-      importToCDN({
-        modules: [
-          {
-            name: "react",
-            var: "React",
-            path: `umd/react.production.min.js`,
-          },
-          {
-            name: "react-dom",
-            var: "ReactDOM",
-            path: `umd/react-dom.production.min.js`,
-          },
-        ],
-      }),
+export {default as getTestOptions} from "./test";
+export {default as getBuildOptions} from "./build";
+export {default as getPreviewOptions} from "./preview";
+export {default as getServeOptions} from "./serve";
+
+export const plugins = [
+  react(),
+  tsconfigPaths(),
+  importToCDN({
+    modules: [
+      {
+        name: "react",
+        var: "React",
+        path: `umd/react.production.min.js`,
+      },
+      {
+        name: "react-dom",
+        var: "ReactDOM",
+        path: `umd/react-dom.production.min.js`,
+      },
     ],
-    server: getServeOptions(),
-    build: getBuildOptions(rootDir, pageNames),
-    preview: getPreviewOptions(),
-    test: getTestOptions(),
-    
-    // Uncomment this if you are using workers.
-    // worker: {
-    //  plugins: [ nxViteTsPaths() ],
-    // },
-  });
-}
+  }),
+];
