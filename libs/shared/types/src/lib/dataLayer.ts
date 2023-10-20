69163788dda19b746017f800b6ddff2d7e22d356
Yue <davidpchen@liontravel.com>
2023-10-20 16:50:55 +0800

    docs: add comment and remove README content

--- libs/shared/types/src/lib/dataLayer.ts
+++ libs/shared/types/src/lib/dataLayer.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export {}; // Make this file as external module
 
+// 可以參考 - https://github.com/sahava/datalayer-typescript
 declare global {
   interface Window {
     dataLayer?: object[];
